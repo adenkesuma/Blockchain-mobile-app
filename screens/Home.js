c73a9e7b48d3ebc6511ed7cd3c62db0fca734256
adenkesuma <adenfdfd10@gmail.com>
2023-10-06 11:57:54 +0700

    `feat:` add status bar and background

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,47 @@
 import { useState } from 'react'
-import { Text, View, SafeArea, FlatList } from 'react-native'
+import { Text, View, SafeAreaView, FlatList } from 'react-native'
 
-// import file
+// import file in folder
 import { COLORS, NFTData } from '../constants'
+import { FocusedStatusBar, NFTCard, HomeHeader } from '../components'
+
 
 const Home = () => {
   return (
-    <Text>
-        Home
-    </Text>
+    <SafeAreaView style={{ flex: 1 }}>
+        <FocusedStatusBar background={COLORS.primary} /> 
+
+        <View style={{ flex: 1 }}>
+            <View style={{ zIndex: 0 }}>
+                <FlatList 
+                    data={NFTData}
+                    keyExtractor={( item ) => item.id}
+                    showsVerticalScrollIndicator={false}
+                    ListHeaderComponent={<HomeHeader />}
+                    renderItem={({ item }) => {
+                        return (
+                            <Text>
+                                {item.name}
+                            </Text>
+                        )
+                    }}
+                />
+            </View>
+
+            {/* fixed background top header */}
+            <View style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                zIndex: -1
+            }}>
+                <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+                <View style={{ fles: 1, backgroundColor: COLORS.white }} />
+            </View>
+        </View>
+    </SafeAreaView>
   )
 }
 
