2fb65a2846d2c2f2b20c7e6c28be13da0f424b96
adenkesuma <adenfdfd10@gmail.com>
2023-10-06 11:36:03 +0700

    initial react navigate and route

---
+++ screens/Home.js
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { Text, View, SafeArea, FlatList } from 'react-native'
+
+// import file
+import { COLORS, NFTData } from '../constants'
+
+const Home = () => {
+  return (
+    <Text>
+        Home
+    </Text>
+  )
+}
+
+export default Home
