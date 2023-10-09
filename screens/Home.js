bffff4740230cbce7d288017ae30caa49c5d7bfe
adenkesuma <adenfdfd10@gmail.com>
2023-10-09 11:43:41 +0700

    `feat:` fix header searchbar filter

--- screens/Home.js
+++ screens/Home.js
@@ -7,36 +7,52 @@ import { FocusedStatusBar, NFTCard, HomeHeader } from '../components'
 
 
 const Home = () => {
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-        <FocusedStatusBar background={COLORS.primary} /> 
+    const [nftData, setNftData] = useState(NFTData)
 
-        <View style={{ flex: 1 }}>
-            <View style={{ zIndex: 0 }}>
-                <FlatList 
-                    data={NFTData}
-                    keyExtractor={({ id }) => id}
-                    showsVerticalScrollIndicator={false}
-                    ListHeaderComponent={<HomeHeader />}
-                    renderItem={({ item }) => <NFTCard data={item} /> }
-                />
-            </View>
+    const handleSearch = (value) => {
+        if (!value.length) return setNftData(NFTData)
 
-            {/* fixed background top header */}
-            <View style={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                zIndex: -1
-            }}>
-                <View style={{ height: 300, backgroundColor: COLORS.primary }} />
-                <View style={{ fles: 1, backgroundColor: COLORS.white }} />
+        const filteredData = NFTData.filter((item) => 
+            item.name.toLowerCase().includes(value.toLowerCase())
+        )
+
+        if (filteredData.length) {
+            setNftData(filteredData)
+        } else {
+            setNftData(NFTData)
+        }
+    }
+
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <FocusedStatusBar background={COLORS.primary} /> 
+        
+            <View style={{ flex: 1 }}>
+                <View style={{ zIndex: 0 }}>
+                    <FlatList 
+                        data={nftData}
+                        keyExtractor={({ id }) => id}
+                        showsVerticalScrollIndicator={false}
+                        ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
+                        renderItem={({ item }) => <NFTCard data={item} /> }
+                    />
+                </View>
+        
+                {/* fixed background top header */}
+                <View style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    zIndex: -1
+                }}>
+                    <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+                    <View style={{ fles: 1, backgroundColor: COLORS.white }} />
+                </View>
             </View>
-        </View>
-    </SafeAreaView>
-  )
+        </SafeAreaView>
+    )
 }
 
 export default Home
